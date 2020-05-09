6273dfc747aa936fcf1efa14dcd3600a24c49b25
Joseph Khan <joseph.khan@altayyargroup.com>
2020-05-09 23:28:59 +0400

    first commit

---
+++ src/Comp.svelte
@@ -0,0 +1,20 @@
+<style>
+    p {
+        color: green;
+    }
+</style>
+<script>
+    let count = 0;
+    $:doubled = count *2;
+    let content = "This is a <strong>HTML</strong> string";
+
+    $:console.log(`the count is ${count}`);
+
+    const handleClick = () => {
+        console.log('test');
+        count += 1;
+    }
+</script>
+
+<p>My own component {@html content}: {count} {doubled}</p>
+<button on:click={handleClick}>Click Me</button>
